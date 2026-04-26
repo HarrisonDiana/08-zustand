91c89addb875158d05ce060c0259670990b37b42
Harrison Diana <dharrison77777@gmail.com>
2026-04-26 19:43:57 +0200

    add index file

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,13 +1,40 @@
 import { fetchNoteById } from "@/lib/api";
+import type { Metadata } from "next";
 import { HydrationBoundary, QueryClient, dehydrate } from "@tanstack/react-query";
 import NotePreview from "@/components/NotePreview/NotePreview";
 
 type PageProps = {
+  params: Promise<{ id: string }>;
+};
+
+type Props = {
   params: { id: string };
 };
 
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const note = await fetchNoteById(params.id);
+
+  return {
+    title: note.title,
+    description: note.content.slice(0, 160),
+    openGraph: {
+      title: note.title,
+      description: note.content.slice(0, 160),
+      url: `/notes/${params.id}`,
+      images: [
+        {
+          url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+          width: 1200,
+          height: 630,
+          alt: note.title,
+        },
+      ],
+    },
+  };
+}
+
 export default async function NoteModalPage({ params }: PageProps) {
-  const { id } = params;
+  const { id } = await params;
   const queryClient = new QueryClient();
 
   await queryClient.prefetchQuery({
